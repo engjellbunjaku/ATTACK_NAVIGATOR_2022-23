fd6da1bb3be2be8e6d8d45ae350dec5a12476040
Enrik Ademi <60113090+enrikademi@users.noreply.github.com>
2023-01-08 20:23:05 +0100

    Create tooltip.component.spec.ts

---
+++ nav-app/src/app/matrix/technique-cell/tooltip/tooltip.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { TooltipComponent } from './tooltip.component';
+
+describe('TooltipComponent', () => {
+  let component: TooltipComponent;
+  let fixture: ComponentFixture<TooltipComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+    imports: [
+        HttpClientTestingModule 
+      ],
+      declarations: [ TooltipComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TooltipComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
