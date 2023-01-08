8114a27b700223991e801378fbb7ed0c499c244d
Enrik Ademi <60113090+enrikademi@users.noreply.github.com>
2023-01-08 20:08:20 +0100

    Create matrix-side.component.spec.ts

---
+++ nav-app/src/app/matrix/matrix-side/matrix-side.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { MatrixSideComponent } from './matrix-side.component';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+describe('MatrixSideComponent', () => {
+  let component: MatrixSideComponent;
+  let fixture: ComponentFixture<MatrixSideComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+    imports: [
+        HttpClientTestingModule,
+        MatDialogModule 
+      ],
+      declarations: [ MatrixSideComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MatrixSideComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+    
